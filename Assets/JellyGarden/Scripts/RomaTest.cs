eafb1dfb379cfa6d11702b2b4aee13021e534a25
Roman <rio900@mail.ru>
2016-06-23 16:59:00 +0300

    Заливка RomaTest  скрипта

---
+++ Assets/JellyGarden/Scripts/RomaTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class RomaTest : MonoBehaviour
+{
+     
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5);
+
+        Debug.Log("Start");
+        for (int i = 0; i < 7; i++)
+        {
+            InitScript.Instance.BuyBoost((BoostType)i, 0, 10);
+        }
+        InitScript.Instance.BuyBoost(BoostType.Random_color, 100, 10);
+        // GameObject.FindObjectsOfType<InitScript>().ReloadBoosts();
+        // PlayerPrefs.DeleteAll();
+        //foreach (var item in GameObject.FindObjectsOfType<BoostIcon>())
+        //{
+        //    item.ActivateBoost();
+        //    Debug.Log("GameObject.FindObjectsOfType<BoostIcon>()");
+        //}
+        //  ActivateBoost();
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
