8aeba9610a183f6a5424849c5d296fe81a2676a6
Roman <rio900@mail.ru>
2016-07-26 15:17:30 +0300

    UI and Dragons folder

--- Assets/JellyGarden/SmartLevelsMap/Scripts/IMapProgressManager.cs
+++
@@ -1,6 +0,0 @@
-﻿public interface IMapProgressManager
-{
-    int LoadLevelStarsCount(int level);
-    void SaveLevelStarsCount(int level, int starsCount);
-    void ClearLevelProgress(int level);
-}
--- Assets/JellyGarden/SmartLevelsMap/Scripts/TranslationType.cs
+++
@@ -1,6 +0,0 @@
-﻿
-public enum TranslationType
-{
-    Teleportation,
-    Walk
-}
